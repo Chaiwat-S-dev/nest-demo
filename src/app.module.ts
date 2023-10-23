2764c86e51b5dab38264e6ad59d9413a708365e7
chaiwat-S-dev <kenkent122@gmail.com>
2023-10-23 16:18:52 +0700

    feat: config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import config from 'config/configuration';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config]
+    }),
     UserModule,
     PostModule
   ],
